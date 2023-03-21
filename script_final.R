30aff211348c6cb0f6759301d63469e87e2bd3e5
Pedro Fernandes <pjf_fernandes@yahoo.com.br>
2023-03-21 11:36:01 -0300

    done

---
+++ script_final.R
@@ -0,0 +1,57 @@
+library("readxl")
+library("tidyverse")
+library("formattable")
+library("openxlsx")
+
+#####Lendo tabela
+df <- read_xlsx("/home/uff/Downloads/DADOS - ESTAÇÃO CAMPOS DOS GOYTACAZES.xlsx")
+
+#####Pegando limiares da sheet de Média e transformando em um vetor
+limiares_df <- read_xlsx("/home/uff/Downloads/DADOS - ESTAÇÃO CAMPOS DOS GOYTACAZES.xlsx", sheet="MÉDIA")
+v <- as.vector(limiares_df[,16])[[1]][2:13]
+
+#####Pegando os nomes das sheets
+sheets <- excel_sheets("/home/uff/Downloads/DADOS - ESTAÇÃO CAMPOS DOS GOYTACAZES.xlsx")
+year_sheets <- numeric()
+
+for (i in 1:length(sheets)) {
+  if(is.numeric(as.numeric(sheets[i]))) {
+    year_sheets <- c(year_sheets, as.numeric(sheets[i]))
+  }
+}
+
+year_sheets <- year_sheets[complete.cases(year_sheets)]
+
+#####Eliminando cabeçalho
+df_no_header <- df[8:nrow(df), ]
+head(df_no_header)
+tail(df_no_header)
+
+####Vetor com as colunas referentes aos dados
+colunas <- seq(2, 35, by=3)
+
+# write dataset
+wb <- createWorkbook()
+
+for (i in 1:length(year_sheets)) {
+  df <- read_xlsx("/home/uff/Downloads/DADOS - ESTAÇÃO CAMPOS DOS GOYTACAZES.xlsx", sheet=as.character(year_sheets[i]))
+ 
+  df_no_header <- df[8:nrow(df), ]
+  head(df_no_header)
+  tail(df_no_header)
+ 
+  addWorksheet(wb, sheetName=as.character(year_sheets[i]))
+ 
+  for (j in 1:length(colunas)) {
+    writeData(wb, sheet=as.character(year_sheets[i]), startCol = j, x = df_no_header[,colunas[j]])
+    #y <- which(colnames(df_no_header[,colunas[j]]) == names(df_no_header[,colunas[j]])[1])
+   
+    x <- which(df_no_header[,colunas[j]] <= v[j])
+   
+    yellow_style <- createStyle(fgFill="#FFFF00")
+    addStyle(wb, sheet=as.character(year_sheets[i]), style=yellow_style, rows=x+1, cols=j, gridExpand=TRUE)
+  }
+}
+
+saveWorkbook(wb, "resultado.xlsx", overwrite=TRUE)
+
